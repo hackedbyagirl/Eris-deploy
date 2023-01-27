04c870925e21f7518af910e3840d028291f83678
Brette <56555806+hackedbyagirl@users.noreply.github.com>
2023-01-27 14:05:00 -0500

    restructure; update variable definitions and values

--- modules/aws/ocon-shared-services/variables.tf
+++ modules/aws/ocon-shared-services/variables.tf
@@ -1,4 +1,19 @@
 # ------------------------------------------------------------------------------
+#                             REQUIRED PARAMETERS
+# ------------------------------------------------------------------------------
+# This bucket is created by cisagov/findings-data-import-terraform.
+variable "assessment_findings_bucket_name" {
+  type        = string
+  description = "The name of the assessment findings S3 bucket."
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to all AWS resources created."
+  default     = {}
+}
+
+# ------------------------------------------------------------------------------
 #                                  AWS Configuration
 # ------------------------------------------------------------------------------
 
@@ -8,58 +23,8 @@ variable "aws_region" {
   default     = "us-west-1"
 }
 
-variable "tags" {
-  type        = map(string)
-  description = "Tags to apply to all AWS resources created."
-  default     = {}
-}
-
 # ------------------------------------------------------------------------------
 #                           AWS Backend Configuration
-# ------------------------------------------------------------------------------
-
-variable "s3_bucket" {
-  type        = string
-  description = "The AWS S3 Bucket"
-  default     = "camelot-ocon-terraform-state"
-}
-
-variable "s3_profile" {
-  type        = string
-  description = "AWS S3 Bucket Profile"
-  default     = "ocon-terraform-backend"
-}
-
-variable "s3_key" {
-  type        = string
-  default     = "ocon-accounts/master.tfstate"  
-}
-
-variable "dynamodb_name" {
-  type        = string
-  description = "AWS DynamoDB Table"
-  default     = "terraform-state-lock"
-}
-
-# This bucket is created by cisagov/findings-data-import-terraform.
-variable "assessment_findings_bucket_name" {
-  type        = string
-  description = "The name of the assessment findings S3 bucket."
-}
-
-# ------------------------------------------------------------------------------
-#                         IAM Documents, Policies, and Roles
-# ------------------------------------------------------------------------------
-# This bucket is created by cisagov/findings-data-import-terraform.
-variable "assessment_findings_bucket_name" {
-  type        = string
-  description = "The name of the assessment findings S3 bucket."
-}
-
-# ------------------------------------------------------------------------------
-# OPTIONAL PARAMETERS
-#
-# These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
 
 variable "assessment_findings_bucket_object_key_pattern" {
@@ -68,19 +33,29 @@ variable "assessment_findings_bucket_object_key_pattern" {
   default     = "*-data.json"
 }
 
-variable "assessment_findings_bucket_write_role_description" {
+# ------------------------------------------------------------------------------
+#                         IAM Roles, Policies, and Documents
+# ------------------------------------------------------------------------------
+
+# Role: WriteAssessmentFindings
+variable "write_assessment_findings_role_name" {
+  type        = string
+  description = "The name to assign the IAM role that allows write access to the assessment findings S3 bucket."
+  default     = "WriteAssessmentFindings"
+}
+
+variable "write_assessment_findings_role_description" {
   type        = string
   description = "The description to associate with the IAM role that allows write access to the assessment findings S3 bucket."
   default     = "Allows write permissions to the assessment findings S3 bucket."
 }
 
-variable "assessment_findings_bucket_write_role_name" {
+# Role: ProvisionOCONServicesAccount
+variable "provisionaccount_role_name" {
   type        = string
-  description = "The name to assign the IAM role that allows write access to the assessment findings S3 bucket."
-  default     = "AssessmentFindingsBucketWrite"
+  description = "The name to assign the IAM role that allows sufficient permissions to provision all AWS resources in the Shared Services account."
+  default     = "ProvisionOCONServicesAccount"
 }
-
-
 
 variable "provisionaccount_role_description" {
   type        = string
@@ -88,26 +63,15 @@ variable "provisionaccount_role_description" {
   default     = "Allows sufficient permissions to provision all AWS resources in the Shared Services account."
 }
 
-variable "provisionaccount_role_name" {
+# Policy: ProvisionSessionManager
+variable "provision_sessionmanager_policy_name" {
   type        = string
-  description = "The name to assign the IAM role that allows sufficient permissions to provision all AWS resources in the Shared Services account."
-  default     = "ProvisionAccount"
+  description = "The name to assign the IAM policy that allows sufficient permissions to provision the SSM Document resource and set up SSM session logging in the Shared Services account."
+  default     = "ProvisionSessionManager"
 }
 
-variable "provisionssmsessionmanager_policy_description" {
+variable "provision_sessionmanager_policy_description" {
   type        = string
   description = "The description to associate with the IAM policy that allows sufficient permissions to provision the SSM Document resource and set up SSM session logging in the Shared Services account."
   default     = "Allows sufficient permissions to provision the SSM Document resource and set up SSM session logging in the Shared Services account."
-}
-
-variable "provisionssmsessionmanager_policy_name" {
-  type        = string
-  description = "The name to assign the IAM policy that allows sufficient permissions to provision the SSM Document resource and set up SSM session logging in the Shared Services account."
-  default     = "ProvisionSSMSessionManager"
-}
-
-variable "tags" {
-  type        = map(string)
-  description = "Tags to apply to all AWS resources created."
-  default     = {}
 }
